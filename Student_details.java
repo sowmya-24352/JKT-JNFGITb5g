3fe98314cefa1ebe268ff2c9fa6306ca98b920b4
sowmya-24352 <yakambariy@gmail.com>
2025-02-07 10:44:55 +0530

    Add files via upload

---
+++ Student_details.java
@@ -0,0 +1,7 @@
+public class Student_details{
+public static void main(String[] args){
+System.out.println("Name:Y.Sowmya");
+System.out.println("Section:C");
+System.out.println("Roll no:24352");
+}
+}
