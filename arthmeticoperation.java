4e10a0064385614e52d18f70be85dce454ec486c
sowmya-24352 <yakambariy@gmail.com>
2025-02-11 20:31:07 +0530

    Add files via upload

---
+++ arthmeticoperation.java
@@ -0,0 +1,10 @@
+class arthmeticoperation {
+   public static void main(String[] args) {
+      int a=3, b=5;
+      System.out.println("a + b = " + (a + b));
+      System.out.println("a - b = " + (a - b));
+      System.out.println("a * b = " + (a * b));
+      System.out.println("a / b = " + (a / b));
+      System.out.println("a % b = " + (a % b));
+}
+}
