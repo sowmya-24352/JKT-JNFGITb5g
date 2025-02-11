2b833259971e6363bdcdad248858f0ade5a972d8
sowmya-24352 <yakambariy@gmail.com>
2025-02-11 20:35:29 +0530

    Add files via upload

---
+++ StringConcatenation.java
@@ -0,0 +1,9 @@
+class StringConcatenation {
+    public static void main(String[] args) {
+    	
+        Double number = -10.6;
+    	
+        System.out.println("I am " + "awesome.");
+        System.out.println("Number = " + number);
+    }
+}
---
+++ variable.java
@@ -0,0 +1,7 @@
+class print varible {
+public static void main(String[] args){
+Double number=-10.6
+System.out.println("I am"+"awesome");
+System.out.println("Number="+number);
+}
+}
